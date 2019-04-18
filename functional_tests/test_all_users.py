614336fec06b535b2006717ef27f6162ba000296
Petr <286109@gmail.com>
2019-04-18 12:00:19 -0700

    Robots.txt and humans.txt view/url/test

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -24,3 +24,9 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
         h1 = self.browser.find_element_by_tag_name("h1")
         self.assertEqual(h1.value_of_css_property("color"), 
                          "rgba(200, 50, 255, 0.9)")
+    
+    def test_home_files(self):
+        self.browser.get(self.live_server_url + "/robots.txt")
+        self.assertNotIn("not found", self.browser.title.lower())
+        self.browser.get(self.live_server_url + "/humans.txt")
+        self.assertNotIn("not found", self.browser.title.lower())
